2e5c053eec91ef119b40d0764c30bc5a0779a19e
a.repin <a.repin@securitycode.ru>
2017-09-25 16:53:23 +0300

    page_at_once

--- p_htmldownload.cpp
+++ p_htmldownload.cpp
@@ -2,10 +2,71 @@
 //
 
 #include "stdafx.h"
+#include <urlmon.h>
 
+#include <fstream>
 
-int main()
+#pragma comment( lib, "urlmon.lib" )
+
+namespace page
 {
-    return 0;
+	void download(
+		_in cstr_t url
+	) {
+		//assert( url );
+
+		string_t url_full( L"http://" );
+		url_full += url;
+		url_full.push_back( L'/' );
+		url = url_full.c_str();
+
+		wprintf_s( L"%s( \"%s\" )...", __FUNCTIONW__, url );
+
+		auto hr = URLDownloadToFile( nullptr, url, OFILE_NAME, 0, nullptr );		
+		if ( S_OK != hr )
+		{
+			wprintf_s( L"URLDownloadToFile(): %08X\n", hr );
+			throw hr;
+		}
+
+		wprintf_s( L" ok\n" );
+	}
+
+	unsigned get_size(
+	) {
+		
+		wprintf_s( L"%s( \"%s\" )...", __FUNCTIONW__, OFILE_NAME );
+		std::wifstream f( OFILE_NAME );
+	
+		if ( !f.is_open() )
+			throw GetLastError();
+	
+		f.seekg( 0, std::ios_base::end );
+		auto size = f.tellg();
+		f.close();
+
+		wprintf_s( L" ok, size: %u bytes\n", static_cast< unsigned >(size) );
+
+		return size;
+	}
+
+}
+
+int wmain(
+	_in size_t arg_c, _in cstr_t arg_v[]
+) {
+    
+	try {
+		assert( 2 == arg_c );
+
+		page::download( arg_v[1] );
+		auto size = page::get_size();
+	}
+	catch (...) {
+		wprintf_s( L"exception: -1\n" );
+		return -1;
+	}
+
+	return 0;
 }
 
--- stdafx.h
+++ stdafx.h
@@ -8,8 +8,17 @@
 #include "targetver.h"
 
 #include <stdio.h>
-#include <tchar.h>
+#include <string>
+#include <assert.h>
 
+#define _in
+#define _out
 
+typedef wchar_t char_t;
+typedef char_t *str_t;
+typedef const char_t *cstr_t;
+
+typedef std::wstring string_t;
 
 // TODO: Установите здесь ссылки на дополнительные заголовки, требующиеся для программы
+#define OFILE_NAME		L"result.html"
